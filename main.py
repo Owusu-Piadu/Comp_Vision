466eeb3fa0520e3a59bc7d4ba3dddb238875a3da
Tarirohope <Priscilakapuyanyika974@gmail.com>
2024-08-15 17:47:00 -0400

    Setting up the environment, Modification

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+# Setting Up Title
+st.title('Brain MRI tumor detection')
+
+# Setting Header
+st.header('Please upload an image')
+
+# Upload File
+
+# Load Model
+
+# Load Image
+
+# Detect Objects
+
+# Visualize
