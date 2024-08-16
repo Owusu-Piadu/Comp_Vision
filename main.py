23ce951c383eedc4e407ed11c24f1d0fe7979679
Tarirohope <Priscilakapuyanyika974@gmail.com>
2024-08-16 10:49:39 -0400

    Setting up the environment, Modification

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ st.title('Brain MRI tumor detection')
 st.header('Please upload an image')
 
 # Upload File
+file = st.file_uploader('', type=['png', 'jpg', 'jpeg'])
 
 # Load Model
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file('COCO-Detection/retinanet_R_101_FPN_3x.yaml'))
+cfg.MODEL.WEIGHTS = './model/model.pth'
+cfg.MODEL.DEVICE = 'cpu'
 
 # Load Image
 
